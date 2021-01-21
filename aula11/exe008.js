a48ab908ef141a328121a8dfc377bbfeb1203de1
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-01-21 12:39:47 -0300

    commit exercicios
    exercicios 08 09 JS CEV

---
+++ aula11/exe008.js
@@ -0,0 +1,9 @@
+console.log('Hello, World!!')
+console.log('O console funcionou perfeitamente')
+var vel = 100.0
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60){ //condição simples
+    console.log('Você ultrapassou a velocidade permitida. Multado!')
+}
+
+console.log('Dirija com cuidado!')
