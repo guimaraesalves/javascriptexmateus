4ff0656f5ed1d589e1ca82e4a600722575c46f0e
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-01-25 15:43:22 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,32 @@
+// Teste no final
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 12)
+
+
+
+
+
+
+
+// Teste no início
+/* 
+var c = 1
+while (c <= 10) {
+    console.log(`Tudo bem? nº ${c}`)
+    c++
+} 
+*/
+
+
+// sem laço de repetição
+/* 
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
