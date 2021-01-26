0f1b5f932868d971fffb77497694989bca2d12ee
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-01-26 10:43:30 -0300

    commit2 quiz
    código de um quiz de multiplas escolhas com JS, CSS e HTML

---
+++ Estudos_livres/question.js
@@ -0,0 +1,44 @@
+var questions = [{
+    "question": "The first mechanical computer designed by Charles Babbage was called?",
+    "option1":"Abacus",
+    "option2": "Analytical Engine",
+    "option3": "Calculator",
+    "option4": "Processor",
+    "answer": "2"
+  }, {
+    "question": "Wich of the following is the most powerful type or computer ?",
+    "option1": "Super-micro",
+    "option2": "Super conductor",
+    "option3": "Super computer",
+    "option4": "MegaFrame",
+    "answer": "3"
+  }, {
+    "question": "Wich is a single integrate circuit?",
+    "option1": "Gate",
+    "option2": "Mother Board",
+    "option3": "Chip",
+    "option4": "CPU",
+    "answer": "1"
+  }, {
+    "question": "C is ?",
+    "option1": "A third generation high level language",
+    "option2": "A machine language",
+    "option3": "An assembly language",
+    "option4": "All of the above",
+    "answer": "1"
+  }, {
+    "question": "Web pages are written using ?",
+    "option1": "FTP",
+    "option2": "HTTP",
+    "option3": "HTML",
+    "option4": "URL",
+    "answer": "3"
+  }, {
+    "question": "UNIVAC is an example of",
+    "option1": "First generation computer",
+    "option2": "Second generation computer",
+    "option3": "Third generation computer",
+    "option4": "Fourth generation computer",
+    "answer": "1"
+  }
+]
