f3094d7d8ead5d9bc9422c989c61de7d3eb10941
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-04 18:39:43 -0300

    Update imc.js

--- Estudos_livres/IMC/imc.js
+++ Estudos_livres/IMC/imc.js
@@ -3,12 +3,18 @@ const calcular = document.getElementById('calcular');
 
 
 function imc () {
-    const nome = document.getElementById('nome');
-    const altura = document.getElementById('altura');
-    const peso = document.getElementById('peso');
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        alert ('Tudo preenchido!')
+    }else{
+        alert ("Preencher todos os campos!!")
+    }
     
 
     }
-}
+
 calcular.addEventListener('click', imc)
