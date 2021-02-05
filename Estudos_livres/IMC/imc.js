cd74e851a9eea42f307acd162dfc50cd44f72ecc
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-05 20:01:26 -0300

    Update imc.js

--- Estudos_livres/IMC/imc.js
+++ Estudos_livres/IMC/imc.js
@@ -7,9 +7,29 @@ function imc () {
     const altura = document.getElementById('altura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
+    var imc = peso / (altura * altura);
 
     if (nome !== '' && altura !== '' && peso !== '') {
-        alert ('Tudo preenchido!')
+        
+        if (imc <= 18.5){
+            alert("Magrela [magreza]")
+        } 
+        if (18.5 < imc <= 24.9) {
+            alert("Tá bem hein? [normal]")
+        }
+        if (25,0 < imc <= 29.90){
+            alert("Gordinho [sobrepeso]")
+        }
+        if (30.0 < imc < 39.9){
+            alert("Balofo! [obesidade]")
+        }
+        if(imc > 40) {
+            alert("Ô baleia! Você vai Morrer![obesidade grave]")
+        }
+
+
+
+
     }else{
         alert ("Preencher todos os campos!!")
     }
