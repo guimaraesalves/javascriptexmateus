e9107d5d6375646619c2aaf1f8bde4c855972b3e
Mateus Guimarães Alves <guimaraesalves45@gmail.com>
2021-02-16 23:03:12 -0300

    commit estudo JS
    create JavaScript  estudo semaforo

---
+++ Estudos_livres/semaforo/semaforo.js
@@ -0,0 +1,47 @@
+const img = document.getElementById('img');
+const buttons = document.getElementById('buttons');
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = (event) => {
+    stopAutomatic();
+    turnOn[event.target.id]();
+    
+}
+
+const nextIndex = () => colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+   //mesma coisa
+    /*if (colorIndex < 2) {
+        colorIndex++
+    }else {
+        colorIndex = 0;
+    }*/
+
+
+
+
+
+const changecolor = () =>{
+    const colors = ['green', 'yellow', 'red']
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
+
+}
+
+const stopAutomatic = () => {
+    clearInterval(intervalId);
+
+}
+
+const turnOn = {
+    'red':      () => img.src = './img/semaforo-vermelho.jpg',
+    'yellow':   () => img.src = './img/semaforo-amarelo.jpg',
+    'green':    () => img.src = './img/semaforo-verde.jpg',
+    'automatic':() => intervalId = setInterval(changecolor, 1000)
+}
+
+
+
+buttons.addEventListener('click', trafficLight);
+
